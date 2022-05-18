662f551e3ca7c4071a41431033a5bf6417dc1cbd
adambujKellogg <adam.bujnowski@kellogg.northwestern.edu>
2022-05-18 02:45:53 +0000

    Final push

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,14 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
+        @post["place_id"] = params["place_id"]
       end
-    
+      def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/places/#{@post["place_id"]}"
+    end
 end
